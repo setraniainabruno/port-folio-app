447c46260d162c6b991c6516ff3d877574096e08
setraniainabruno <brunoharison18@yahoo.com>
2025-07-25 13:33:34 +0300

    Corriger utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,20 +1,15 @@
 import axios from 'axios';
 
 
-export const api: any = () => {
-  axios.create({
-    baseURL: 'https://monportfolio-str-api.onrender.com/api'
-  });
-}
+export const api = axios.create({
+  baseURL: 'https://monportfolio-str-api.onrender.com/api'
+});
 
-export const api_verify_mail: any = () => {
-  axios.create({
-    baseURL: 'https://api.hunter.io/v2'
-  });
-}
+export const api_verify_mail = axios.create({
+  baseURL: 'https://api.hunter.io/v2',
+});
 
-export const api_key: any = () => {
-  return '8a3994eae747af08965f619e9eb019f3cd89f3c9';
-}
+export const api_key = '8a3994eae747af08965f619e9eb019f3cd89f3c9';
+
 
 
